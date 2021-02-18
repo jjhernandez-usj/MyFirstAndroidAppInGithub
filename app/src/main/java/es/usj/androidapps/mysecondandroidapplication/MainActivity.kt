9d9ad34b6b1213c151cd13704dc8d89608c58e09
jhernandez-movicoders <juanjo.hernandez@movicoders.com>
2021-02-18 20:23:15 +0100

    main: Hide/show the text view implemented.

--- app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
@@ -2,16 +2,26 @@ package es.usj.androidapps.mysecondandroidapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var tvVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView.visibility = TextView.INVISIBLE
+        button.setOnClickListener { toggleVisibility() }
         // This is a comment to show how it is pushed to the cloud
         // Chat is working. Hooray!!!
+    }
+
+    private fun toggleVisibility() {
+        val visibility = if(tvVisible) TextView.INVISIBLE else TextView.VISIBLE
+        tvVisible = !tvVisible
+        textView.visibility = visibility
     }
 }
