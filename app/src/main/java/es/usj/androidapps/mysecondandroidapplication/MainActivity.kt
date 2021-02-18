b5828dba7c8eaef19651208b22d12027ded4e852
jhernandez-movicoders <juanjo.hernandez@movicoders.com>
2021-02-18 19:07:49 +0100

    main: first comment in code.

--- app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // This is a comment to show how it is pushed to the cloud
     }
 }
