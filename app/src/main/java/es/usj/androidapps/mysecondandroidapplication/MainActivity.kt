1798ede39bbf96f0819f65fe0f6317ca64e4da5a
jhernandez-movicoders <juanjo.hernandez@movicoders.com>
2021-02-18 20:11:50 +0100

    main: Hide the text view.

--- app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
@@ -3,10 +3,14 @@ package es.usj.androidapps.mysecondandroidapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.visibility = TextView.INVISIBLE
         // This is a comment to show how it is pushed to the cloud
         // Chat is working. Hooray!!!
     }
