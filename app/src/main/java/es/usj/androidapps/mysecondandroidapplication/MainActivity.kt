11451b7d2c5b65d2e8bff737fda8e3afc3532e20
jhernandez-movicoders <juanjo.hernandez@movicoders.com>
2021-02-18 19:05:02 +0100

    main: first commit.

---
+++ app/src/main/java/es/usj/androidapps/mysecondandroidapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.usj.androidapps.mysecondandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
